a59de331cfa7aa957b1c733dd249d90a42c04229
Dilan Jimenez Arroyave <dilanjimenez140@gmail.com>
2022-08-15 15:57:58 -0500

    Agregar Menú tipo Mobile y algunas Media Queries

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerIconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerIconMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
   //Poniendo o quitando clase .inactive cada vez que se haga click
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('inactive');
 }
